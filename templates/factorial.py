acb3255b4165e354a49ee8d62848d9f4a5157920
shrutiapcsp <shrutijchari@gmail.com>
2022-03-28 16:58:02 -0700

    week 2

---
+++ templates/factorial.py
@@ -0,0 +1,26 @@
+
+# Python 3 program to find
+# factorial of given number
+def factorial(n):
+
+    # Checking the number
+    # is 1 or 0 then
+    # return 1
+    # other wise return
+    # factorial
+    if (n==1 or n==0):
+
+        return 1
+
+    else:
+
+        return (n * factorial(n - 1))
+
+def Test():
+    # Driver Code
+    num = 5;
+    print("number : ",num)
+    print("Factorial : ",factorial(num))
+
+if __name__ == "__main__":
+    Test()
